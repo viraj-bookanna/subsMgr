0056dc408ac04ab4347135dbcb1009f48c68dac7
Indika Viraj <samangoonerathne@gmail.com>
2025-04-03 05:05:04 +0000

    string list

---
+++ strings.py
@@ -0,0 +1,8 @@
+direct_reply = {
+    '/start': "Welcome!",
+    '/join': "Join the channel to get the invite link",
+}
+
+strings = {
+    'unknown_command': "Unknown command, please try again",
+}
